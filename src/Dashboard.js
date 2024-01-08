11d2990c51f5d1667207297977dad305eb95d945
Alok Dubey <alok.dubey22@spit.ac.in>
2024-01-08 17:45:51 +0530

    adding dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,8 +4,18 @@ import "./Dashboard.css";
 const Dashboard = () => {
   return (
     <div className="dashboard">
-      <p>Temporary image. w'll change it later </p>
-      <img src="./dashboard.png" alt="dashboard image" />
+      {/* <p>Temporary image. w'll change it later </p>
+      <img src="./dashboard.png" alt="dashboard image" /> */}
+      <div className="upper">
+        <div className="internal-box">Total revenue</div>
+        <div className="internal-box">Total job</div>
+        <div className="internal-box">ticket</div>
+        <div className="internal-box">ticket schedule</div>
+        <div className="internal-box">outstanding amount</div>
+        <div className="internal-box">membership</div>
+        <div className="internal-box">job completed</div>
+        <div className="internal-box">total canceled</div>
+      </div>
     </div>
   );
 };
